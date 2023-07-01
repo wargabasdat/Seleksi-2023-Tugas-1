f5da16967bea09c2b0b7ed9cbf44d14d618b95be
Fatih20 <fnri39@protonmail.com>
2023-07-01 12:24:55 +0700

    Change in library.

--- Data Scraping/src/allProblems.js
+++ Data Scraping/src/allProblems.js
@@ -1,12 +1,33 @@
-const rp = require("request-promise");
-const url =
-  "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
+require("events").EventEmitter.defaultMaxListeners = 15;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { wrapper } = require("axios-cookiejar-support");
+const { CookieJar } = require("tough-cookie");
 
-rp(url)
-  .then(function (html) {
-    //success!
-    console.log(html);
+const url = "https://leetcode.com/problemset/all/?page=1";
+
+const jar = new CookieJar();
+const client = wrapper(axios.create({ jar }));
+
+client
+  .get(url, { responseType: "text" })
+  .then(function ({ data: html }) {
+    const $ = cheerio.load(html);
+    // const $title = $("div[role='cell'] span", html);
+    // console.log($title.length);
   })
-  .catch(function (err) {
-    //handle error
+  .catch(function (error) {
+    console.log(error);
   });
+
+// rp(url)
+//   .then(function (html) {
+//     console.log("Bruh");
+//     console.log(html);
+//     // const $row = $("[role='row']", html);
+//     // console.log($row.length);
+//     // console.log($row);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
