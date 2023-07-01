e7793b1a9ae44fef648dd445e80366b70a765eb7
Fatih20 <fnri39@protonmail.com>
2023-07-01 12:41:17 +0700

    Change library to using puppeteer.

--- Data Scraping/src/allProblems.js
+++ Data Scraping/src/allProblems.js
@@ -1,33 +1,37 @@
-require("events").EventEmitter.defaultMaxListeners = 15;
-const axios = require("axios");
 const cheerio = require("cheerio");
-const { wrapper } = require("axios-cookiejar-support");
-const { CookieJar } = require("tough-cookie");
+const puppeteer = require("puppeteer");
 
 const url = "https://leetcode.com/problemset/all/?page=1";
 
-const jar = new CookieJar();
-const client = wrapper(axios.create({ jar }));
+// Get the HTML from the page by using puppeteer
+// The reason puppeteer is needed is because the website uses a framework and isn't static
+async function getHTML() {
+  // Initiate the browser
+  const browser = await puppeteer.launch();
 
-client
-  .get(url, { responseType: "text" })
-  .then(function ({ data: html }) {
-    const $ = cheerio.load(html);
-    // const $title = $("div[role='cell'] span", html);
-    // console.log($title.length);
-  })
-  .catch(function (error) {
-    console.log(error);
+  // Create a new page in headless chrome
+  const page = await browser.newPage();
+
+  // Go to target website
+  // Have it set to wait until the content has fully loaded (no more network activities)
+  await page.goto(url, {
+    waitUntil: "networkidle0",
   });
 
-// rp(url)
-//   .then(function (html) {
-//     console.log("Bruh");
-//     console.log(html);
-//     // const $row = $("[role='row']", html);
-//     // console.log($row.length);
-//     // console.log($row);
-//   })
-//   .catch(function (err) {
-//     console.log(err);
-//   });
+  // Get full page html
+  const html = await page.content();
+
+  // Close headless chrome
+  await browser.close();
+
+  return html;
+}
+
+// Execute the fetching of all of the problems
+(async () => {
+  const html = await getHTML();
+
+  const $ = cheerio.load(html);
+  const $title = $("div[role='cell'] span", html);
+  console.log($title.length);
+})();
