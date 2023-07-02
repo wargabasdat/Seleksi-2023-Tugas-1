226620fc5abf458a03ca87c33d929fc22ce29244
cathlauretta <cathlauretta@gmail.com>
2023-07-03 01:23:30 +0700

    add user-agent headers

--- Data Scraping/src/scraper.py
+++ Data Scraping/src/scraper.py
@@ -12,8 +12,9 @@ for i in range (11) :
         url = "https://kecilkecilpunyakarya.com/shop/"
     else :
         url = "https://kecilkecilpunyakarya.com/shop/page/" + str(i+1) + "/"
-    response = requests.get(url)
-    print(response, ": Berhasil melakukan scrapping dari halaman ke", i + 1)
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers = headers)
+    print(f"{response} : Berhasil melakukan scrapping dari halaman ke-{i + 1}")
     soup = BeautifulSoup(response.content, "html.parser")
 
     produk = soup.find_all("div", "woocommerce-product-inner")
@@ -30,11 +31,11 @@ for i in range (11) :
         # print(book_title, book_genres, book_price)
 
 product_dict = {
-    "Product Name" : title,
+    "Book Title" : title,
     "Genre" : genre,
     "Price" : price
 }
 
-df = pd.DataFrame(product_dict, columns = ["Product Name", "Genre", "Price"])
-df.sort_values("Product Name", ascending = True)
+df = pd.DataFrame(product_dict, columns = ["Book Title", "Genre", "Price"])
+df.sort_values("Book Title", ascending = True)
 print(df)
