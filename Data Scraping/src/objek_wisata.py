dba628baef77fd70d20ae14ad014f973b7861068
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-07-03 23:50:24 +0700

    objek_wisata done scrap

--- Data Scraping/src/objek_wisata.py
+++ Data Scraping/src/objek_wisata.py
@@ -1,7 +1,5 @@
 import requests
 import json
-import pandas
-import csv
 from bs4 import BeautifulSoup
 
 # Url dari website yang ingin di-scraping
@@ -16,7 +14,8 @@ header = {
 # Inisialisasi tempat penyimpanan untuk menyimpan hasil ekstrak data
 list_objek_wisata = []
 page = 0
-while (page <= 180):
+peringkat = 0
+while (page <= 180) :
     # Melakukan request pada website yang akan diekstrak
     url = 'https://www.tripadvisor.co.id/Attractions-g2301784-Activities-oa{}-West_Sumatra_Sumatra.html'.format(page)
     req = requests.post(url, headers = header)
@@ -25,7 +24,6 @@ while (page <= 180):
     # Melakukan ekstrak data
     objek_wisata = soup.findAll('article', 'GTuVU XJlaI')
     for objek in objek_wisata:
-        peringkat = objek.find('div', 'XfVdV o AIbhI').text.split('. ')[0]
         nama_objek = objek.find('div', 'XfVdV o AIbhI').text.split('. ')[1]
         jenis_objek = objek.find('div', 'biGQs _P pZUbB hmDzD').text.replace(' • ', ', ')
         try :
@@ -36,18 +34,34 @@ while (page <= 180):
             jumlah_ulasan = objek.find('span', 'biGQs _P pZUbB osNWb').text
         except :
             jumlah_ulasan = ''
+        
+        # Masuk ke halaman objek untuk mendapatkan informasi daerah tempat objek wisata berada
+        link_objek = 'https://www.tripadvisor.co.id/' + objek.find('div', 'alPVI eNNhq PgLKC tnGGX').find('a')['href']
+        headers_objek = {
+            'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        }
+        req_objek = requests.get(link_objek, headers = headers_objek)
+        soup_objek = BeautifulSoup(req_objek.text, 'html.parser')
+        items = soup_objek.findAll('div', 'IuzAT e')
+        for cek_daerah in items:
+            daerah_objek = cek_daerah.find('div', 'biGQs _P pZUbB KxBGd').text.split('di ')[1]
 
         # Melakukan penyimpanan hasil ekstrak data ke tempat penyimpanan yang telah diinisialisasi sebelumnya
-        hasil_objek_wisata = {
-            'Peringkat' : peringkat,
-            'Nama_Objek' : nama_objek,
-            'Jenis_Objek' : jenis_objek,
-            'Rating' : rating,
-            'Jumlah_Ulasan' : jumlah_ulasan
-        }
-        list_objek_wisata.append(hasil_objek_wisata)
+        # Penyimpanan dilakukan dengan didahului dengan cleaning data dari data kosong
+        if (nama_objek != '' and jenis_objek != '' and rating != '' and jumlah_ulasan != '') :
+            peringkat += 1
+            hasil_objek_wisata = {
+                'Peringkat' : peringkat,
+                'Nama_Objek' : nama_objek,
+                'Jenis_Objek' : jenis_objek,
+                'Daerah Objek' : daerah_objek,
+                'Rating' : rating,
+                'Jumlah_Ulasan' : jumlah_ulasan
+            }
+            list_objek_wisata.append(hasil_objek_wisata)
+        
     page += 30
 
 # Proses formatting hasil ekstrak data ke bentuk json
-with open('Data Scraping/data/hasil.json', 'w') as file:
+with open('Data Scraping/data/objek_wisata.json', 'w') as file:
     json.dump(list_objek_wisata, file, indent = 5)
