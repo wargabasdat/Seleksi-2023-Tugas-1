ca70ab59b50470a7d5dd0b2fa20be649269d6f7a
adtyainash <16521107@mahasiswa.itb.ac.id>
2023-07-05 09:16:55 +0700

    push

--- datascrap.py
+++ datascrap.py
@@ -1,13 +1,50 @@
+# import requests
+# from bs4 import BeautifulSoup
+ 
+# url = 'https://reviews.femaledaily.com/products/lips/lipstick?brand=&order=popular&page=1'
+# # headers = {
+# #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+# # }
+# req = requests.get(url)
+# soup = BeautifulSoup(req.text, 'html.parser')
+# items = soup.findAll('div', 'jsx-2856971448 product-item')
+# print(items)
+# for it in items:
+#     name = it.find('h2', 'jsx-2059197805 product-card-catalog-title').text
+#     print(name)
+
 import requests
 from bs4 import BeautifulSoup
+import csv
  
-url = 'https://reviews.femaledaily.com/products/lips/lipstick?brand=&order=popular&page=1'
-# headers = {
-#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-# }
-req = requests.get(url)
-soup = BeautifulSoup(req.text, 'html.parser')
-items = soup.findAll('div', 'jsx-2856971448 product-item')
-for it in items:
-    name = it.find('h2', 'jsx-2059197805 product-card-catalog-title').text
-    print(name)
+key = input('please enter the term :')
+location = input('please enter the location too :')
+url = 'https://www.yell.com/ucs/UcsSearchAction.do?scrambleSeed=1673388186&keywords={}&location={}&pageNum='.format(key, location)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+}
+ 
+datas = []
+count_page = 0
+for page in range(1, 11):
+    count_page+=1
+    print('scraping page :',count_page)
+    req = requests.get(url+str(page), headers=headers)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    items = soup.findAll('div', 'row businessCapsule--mainRow')
+    for it in items:
+        name = it.find('span', 'businessCapsule--name').text
+        try : address = ''.join(it.find('span', {'itemprop':'address'}).text.strip().split('\n'))
+        except : address = ''
+        try : web = it.find('a', {'rel':'nofollow noopener'})['href'].replace('http://', '').replace('www.', '').replace('https://', '').split('/')[0]
+        except : web = ''
+        try : telp = it.find('span', 'business--telephoneNumber').text
+        except: telp= ''
+        image = it.find('div', 'col-sm-4 col-md-4 col-lg-5 businessCapsule--leftSide').find('img')['data-original']
+        if 'http' not in image: image = 'https://www.yell.com{}'.format(image)
+        datas.append([name, address, web, telp, image])
+ 
+kepala = ['Name', 'Address', 'Website', 'Phone Number', 'Image URL']
+writer = csv.writer(open('results/{}_{}.csv'.format(key, location), 'w', newline=''))
+writer.writerow(kepala)
+for d in datas: writer.writerow(d)
--- datascrap2.py
+++ datascrap2.py
@@ -1,14 +1,37 @@
 import requests
+import json
 from bs4 import BeautifulSoup
  
-url = 'https://www.zalora.co.id/c/beauty/make-up/c-7968?page=2'
-# headers = {
-#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-# }
-req = requests.get(url)
-print(req)
-# soup = BeautifulSoup(req.text, 'html.parser')
-# items = soup.findAll('div', 'jsx-2856971448 product-item')
-# for it in items:
-#     name = it.find('h2', 'jsx-2059197805 product-card-catalog-title').text
-#     print(name)
+url = 'https://go.drugbank.com/drugs?approved=1&c=name&d=up&page='
+data = []
+count_page = 0
+for page in range(1, 3):
+    count_page+=1
+    print('scraping page :',count_page)
+    req = requests.get(url+str(page))
+    soup = BeautifulSoup(req.text, 'html.parser')
+    items = soup.findAll('tr')
+    for it in items:
+        name = it.find('a').text
+        try : weight = it.find('td', class_='weight-value').text.split()[0]
+        except : weight = ''
+        try : description = it.find('td', class_='description-value').text
+        except : description = ''
+        try : categories = it.find('td', class_='categories-value').find('a').text
+        except : categories = ''
+
+        # Create a dictionary for each item
+        if name != 'Name':
+            item_data = {
+                'name': name,
+                'weight': weight,
+                'description': description,
+                'categories': categories
+            }
+
+            # Append the item dictionary to the data list
+            data.append(item_data)
+
+# Save the data as JSON
+with open('result.json', 'w') as file:
+    json.dump(data, file, indent=4)
