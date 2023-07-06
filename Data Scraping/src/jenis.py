9e2b0cdcb5ca1e6b0de0fcf16c7c85bf13242baf
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-07-06 21:54:53 +0700

    Set ulang

---
+++ Data Scraping/src/jenis.py
@@ -0,0 +1,38 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+# Url dari website yang ingin di-scraping
+url = 'https://www.tripadvisor.co.id/Attractions-g2301784-Activities-oa0-West_Sumatra_Sumatra.html'
+
+# Header sebagai identitas dalam melakukan scraping
+header = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+}
+
+# Melakukan request pada website yang akan diekstrak
+req = requests.post(url, headers = header)
+soup = BeautifulSoup(req.text, "html.parser")
+
+# Melakukan ekstrak data
+items = soup.findAll('div', 'XDHza y f u G')
+number = 0
+for it in items:
+    # Cleaning data dilakukan karena item yang ingin diekstrak dari web memiliki class yang sama dengan beberapa item yang tidak diinginkan
+    number += 1
+    if (number <= 6) :
+        continue
+
+    # Lanjut proses ekstrak data
+    try :
+        jenis_objek = it.find('div', 'biGQs _P pZUbB KxBGd').text
+    except :
+        jenis_objek = ''
+    try :
+        kategori = it.find('a', 'KoOWI')['href']
+    except :
+        kategori = ''
+
+    # New url digunakan untuk melihat jumlah 
+    new_url = 'https://www.tripadvisor.co.id/Attractions-g2301784-Activities-oa0-West_Sumatra_Sumatra.html#{}'.format(kategori)
+    print(new_url)
