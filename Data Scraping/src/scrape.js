158b9136189c9be9dd40c84cea1048d943114384
Fatih20 <fnri39@protonmail.com>
2023-07-09 09:09:35 +0700

    Give time information during scraping.

--- Data Scraping/src/scrape.js
+++ Data Scraping/src/scrape.js
@@ -1,8 +1,11 @@
 const scraperProbems = require("./scraper/allProblems");
 const scraperProblemsExtended = require("./scraper/allProblemsExtended");
 const scraperTags = require("./scraper/allTags");
+const msToTime = require("./utilities/msToTime");
 
 (async () => {
+  const start = Date.now();
+  console.log("Starting web scraping");
   // Scrape all of the problems
   await scraperProbems();
 
@@ -11,4 +14,8 @@ const scraperTags = require("./scraper/allTags");
 
   // Scrape the tags and the problem in each of them
   await scraperTags();
+
+  const end = Date.now();
+  const duration = end - start;
+  console.log(`Finished scraping in ${msToTime(duration)}.`);
 })();
