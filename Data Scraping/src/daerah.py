bd851e3fe8e8df51986cfb990f684d1f73410cf0
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-07-10 12:25:06 +0700

    daerah fix

--- Data Scraping/src/daerah.py
+++ Data Scraping/src/daerah.py
@@ -14,7 +14,7 @@ header = {
 list_daerah_objek = []
 
 # Inisialisasi tempat penyimpanan sementara hasil ekstraksi data agar tidak ada duplikat data
-set_daerah_objek = set()
+dict_daerah_objek = dict()
 page = 0
 while (page <= 180) :
     # Melakukan request pada setiap halaman yang akan diekstrak
@@ -25,6 +25,21 @@ while (page <= 180) :
     # Melakukan request pada setiap halaman baru per objek wisata
     objek_wisata = soup.findAll('article', 'GTuVU XJlaI')
     for objek in objek_wisata:
+        nama_objek = objek.find('div', 'XfVdV o AIbhI').text.split('. ')[1]
+        try :
+            nama_jenis = objek.find('div', 'biGQs _P pZUbB hmDzD').text
+        except :
+            nama_jenis = ''
+        try :
+            rata_rata_rating = objek.find('svg', 'UctUV d H0 hzzSG')['aria-label'].split(' ')[0].replace(',', '.')
+        except :
+            rata_rata_rating = ''
+        try :
+            jumlah_ulasan = objek.find('span', 'biGQs _P pZUbB osNWb').text
+        except :
+            jumlah_ulasan = ''
+
+        # Masuk ke halaman setiap objek untuk melihat daerah objek wisata
         url_per_objek_wisata = 'https://www.tripadvisor.co.id/' + objek.find('div', 'alPVI eNNhq PgLKC tnGGX').find('a')['href']
         header_per_objek_wisata = {
             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
@@ -36,15 +51,23 @@ while (page <= 180) :
         items = soup_objek_wisata.findAll('div', 'IuzAT e')
         for cek_daerah in items :
             nama_daerah = cek_daerah.find('div', 'biGQs _P pZUbB KxBGd').text.split('di ')[1]
-            set_daerah_objek.add(nama_daerah)
+            if (nama_objek != '' and nama_daerah != '' and nama_jenis != '' and rata_rata_rating != '' and jumlah_ulasan != '') :
+                if (nama_daerah in dict_daerah_objek) :
+                    dict_daerah_objek[nama_daerah] = dict_daerah_objek[nama_daerah] + 1
+                else :
+                    dict_daerah_objek[nama_daerah] = 1
+            else :
+                if (nama_daerah not in dict_daerah_objek) :
+                    dict_daerah_objek[nama_daerah] = 0
     page += 30
 
 # PENYIMPANAN (PILIH SALAH SATU BENTUK JSON ATAU CSV)
 # AKAN DISIMPAN DALAM BENTUK JSON
 # Simpan data ke dictionary agar bisa dijadiin JSON
-for x in set_daerah_objek :
+for x in dict_daerah_objek :
     hasil_daerah = {
-        "nama_daerah" : x
+        "nama_daerah" : x,
+        "jumlah_objek_sedaerah" : dict_daerah_objek[x]
     }
     list_daerah_objek.append(hasil_daerah)
 
@@ -53,9 +76,9 @@ with open('Data Scraping/data/daerah.json', 'w') as file:
     json.dump(list_daerah_objek, file, indent = 4)
 
 # # AKAN DISIMPAN DALAM BENTUK CSV
-# for x in set_daerah_objek :
-#     list_daerah_objek.append([x])
-# kepala = ['nama_daerah']
+# for x in dict_daerah_objek :
+#     list_daerah_objek.append([x, dict_daerah_objek[x]])
+# kepala = ['nama_daerah', 'jumlah_objek_sedaerah']
 # writer = csv.writer(open('Data Scraping/data/daerah.csv', 'w', newline = ''))
 # writer.writerow(kepala)
 # for data in list_daerah_objek :
