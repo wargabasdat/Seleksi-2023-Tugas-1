6a4e2e3553bb17040b45130cab92749e84c379d9
cathlauretta <cathlauretta@gmail.com>
2023-07-14 14:08:50 +0700

    feat: fix scraper.py and add scraping result in .json

--- Data Scraping/src/scraper.py
+++ Data Scraping/src/scraper.py
@@ -1,59 +1,152 @@
-from book_scraper import *
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
 
+id = []
 title = []
-genre = []
 price = []
+isbn = []
 
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+
+genre_df = pd.DataFrame(columns = ["Book ID", "Genre"])
+author_df = pd.DataFrame(columns = ["Book ID", "Author"])
+
+# Function to adjust the book title to match the product page url
 def adjustment(title) :
     title = title.lower()
     title = title.replace(" ", "-").replace("’", "").replace("!", "").replace(",", "")
     
+    # Adjustment for one product that has typo in the product page url
     if (title == "fairyland-adventure") :
         title = "fairlyland-adventure"
     
     return title
 
+# Function to get the author and isbn of the book from the product page
+def get_item(title, book_id) :
+    url = "https://kecilkecilpunyakarya.com/product/" + title + "/"
+    response = requests.get(url, headers = headers)
+    print(f"{response} : Berhasil melakukan scraping dari produk {title}")
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Each information is stored in a p tag
+    info = soup.find_all('p')
+
+    # Check if the product page contain isbn and author information
+    contain_isbn = False
+    contain_author = False
+
+    for i in info :
+        if ("ISBN: " in i.get_text()) :
+            contain_isbn = True
+
+        if ("Penulis: " in i.get_text()) :
+            contain_author = True
+
+    # Get the author and isbn information
+    # Some product has one or more authors
+    # But each product has only one isbn if it's already been published
+    authors = []
+    for i in info :
+        if (not contain_isbn and not contain_author) :
+            isbn.append(None)
+            break
+        elif (not contain_isbn) :
+            if ("Penulis: " in i.get_text()) :
+                book_author = i.get_text().replace("Penulis: ", "").replace("\u2019", "'").replace(", dkk.", "")
+                authors = book_author.split(", ")
+                isbn.append(None)
+                break
+        elif (not contain_author) :
+            if ("ISBN: " in i.get_text()) :
+                book_isbn = i.get_text().replace("ISBN: ", "")
+                isbn.append(book_isbn)
+                break
+        elif (contain_isbn and contain_author) :
+            if ("Penulis: " in i.get_text()) :
+                book_author = i.get_text().replace("Penulis: ", "").replace("\u2019", "'").replace(", dkk.", "")
+                authors = book_author.split(", ")
+            elif ("ISBN: " in i.get_text()) :
+                book_isbn = i.get_text().replace("ISBN: ", "")
+                isbn.append(book_isbn)
+
+    # If the book have information about the author, add it to the author_df
+    if authors != [] :
+        for a in authors :
+            new_row = [book_id, a]
+            author_df.loc[len(author_df)] = new_row
+
+# Declare variable to count how many books in the catalogue
+count = 0
+
+# KKPK product catalogue consist of 11 web pages
 for i in range (11) :
     if i == 0 :
         url = "https://kecilkecilpunyakarya.com/shop/"
     else :
         url = "https://kecilkecilpunyakarya.com/shop/page/" + str(i+1) + "/"
 
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
     response = requests.get(url, headers = headers)
-    print(f"{response} : Berhasil melakukan scrapping dari halaman ke-{i + 1}")
+    print(f"{response} : Berhasil melakukan scraping dari halaman ke-{i + 1}")
     soup = BeautifulSoup(response.content, "html.parser")
 
+    # Every information of the product is stored in a div with class "woocommerce-product-inner"
     produk = soup.find_all("div", "woocommerce-product-inner")
-    # print(produk)
 
     for p in produk :
-        # print(p)
-        book_title = p.find('h3', 'woocommerce-product-title').get_text().replace("\n", "")
-        book_genres = p.find('div', 'woocommerce-product-category').get_text().replace("\n", "")
+        # There might be a product that has one or more genres
+        genres = []
+
+        book_title = p.find('h3', 'woocommerce-product-title').get_text().replace("\n", "").replace("\u2019", "'")
+        book_genres = p.find('div', 'woocommerce-product-category').get_text().replace("\n", "").replace(" ", "")
         book_price = p.find('span', 'price').get_text().replace("Rp", "").replace(".", "")
+
+        genres = book_genres.split(",")
+
         title.append(book_title)
-        genre.append(book_genres)
-        price.append(book_price)
+        price.append(int(book_price))
+
+        count += 1
+        if (count < 10) :
+            book_id = "BOOK00" + str(count)
+        else :
+            book_id = "BOOK0" + str(count)
+        id.append(book_id)
 
         book_title = adjustment(book_title)
-        get_item(book_title)
-        # print(book_title, book_genres, book_price)
+        get_item(book_title, book_id)
 
-# print(len(title))
-# print(len(genre))
-# print(len(price))
-# print(len(author))
-# print(len(isbn))
+        # If the book have information about the genre, add it to the genre_df
+        for g in genres :
+            new_row = [book_id, g]
+            genre_df.loc[len(genre_df)] = new_row
 
 product_dict = {
+    "Book ID" : id,
     "Book Title" : title,
-    "Genre" : genre,
     "Price" : price,
-    "Author" : author,
     "ISBN" : isbn
 }
 
-df = pd.DataFrame(product_dict, columns = ["Book Title", "Genre", "Price", "Author", "ISBN"])
-df.sort_values("Book Title", ascending = True)
-print(df)
+dir = os.getcwd()
+print(dir)
+
+book_df = pd.DataFrame(product_dict, columns = ["Book ID", "Book Title", "Price", "ISBN"])
+print(len(book_df))
+print(len(genre_df))
+print(len(author_df))
+
+book_df.sort_values("Book ID", ascending = True)
+genre_df.sort_values("Book ID", ascending = True)
+author_df.sort_values("Book ID", ascending = True)
+book_df.to_json(dir[:-3] + 'data\\book_details.json', orient = 'records', indent = 2)
+genre_df.to_json(dir[:-3] + 'data\\book_genre.json', orient = 'records', indent = 2)
+author_df.to_json(dir[:-3] + 'data\\book_author.json', orient = 'records', indent = 2)
+
+# Utk referensi
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_json.html
+# https://docs.devart.com/studio-for-postgresql/exporting-and-importing-data/json-import.html
+# https://www.youtube.com/watch?v=C5AOZZWxvIY
+# https://sparkbyexamples.com/pandas/pandas-add-row-to-dataframe/
